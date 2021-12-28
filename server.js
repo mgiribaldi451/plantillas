7e3f219ebfa8bc05aac0e4cf48bd109e2cddd63e
mgiribaldi <matigiri.1992@gmail.com>
2021-12-27 22:47:08 -0300

    practico plantillas

---
+++ server.js
@@ -0,0 +1,70 @@
+//import handlebars from 'express-handlebars'
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const bp = require('body-parser')
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+
+//establecemos la configuración de handlebars
+app.engine(
+    'hbs',handlebars.engine({
+        extname: ".hbs",
+        defaultLayout: 'index.hbs'
+      })
+);
+
+
+let productos=[
+
+    {
+        "id": 1,
+        "Nombre": "inter",
+        "Url": "https://i.ibb.co/dgjzjvK/images-q-tbn-ANd9-Gc-TUtrjy-hxo-S7itymm994-PWb1-o-SLUZMo38-Fg-usqp-CAU.jpg",
+        "Precio": 2000
+    },
+    {
+        "id": 2,
+        "Nombre": "defensa y justicia",
+        "Url": "https://i.ibb.co/WBzHNLv/images-q-tbn-ANd9-Gc-Rxu-I5t-AXdtt-Izamd-RH9-AMzkt-WTbk-Opk-TRk-Jg-usqp-CAU.jpg",
+        "Precio": 1500
+
+    },
+    {
+        "id": 3,
+        "Nombre": "atletico madrid",
+        "Url": "https://i.ibb.co/Z2zDQ6y/images-q-tbn-ANd9-Gc-Re-FPQu-FEjev-D6-YS2-NTIug-Fx-X-fsjkr-Jucp-BQ-usqp-CAU.jpg",
+        "Precio": 2500
+    }
+]
+
+app.get("/productos", (req, res) => {
+    res.render("datos", {productos:productos,listExists:true});
+});
+
+
+const nom = null;
+app.get('/', (req, res) => {
+    res.render("form")
+})
+
+app.post('/productos', (req, res) => {
+    
+    const valor = Math.max(...productos.map(o => o.id), 0);
+    console.log(req.body);
+    req.body.id = valor + 1
+    productos = [...productos, req.body]
+    res.render("datos", {productos:productos,listExists:true});
+})
+
+/* ------------------------------------------------------ */
+/* Server Listen */
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor ${error}`))
